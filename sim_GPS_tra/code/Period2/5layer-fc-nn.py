a7e29d1a73200d4c8e173fdf9b37e42401c13511
Vincent_Yao <vincent_yao1995@foxmail.com>
2018-05-03 22:19:47 +0800

    code debug done, time to change dataset and try more test, now 0.0 acc and loss are gradually increasing

--- sim_GPS_tra/code/Period2/5layer-fc-nn.py
+++ sim_GPS_tra/code/Period2/5layer-fc-nn.py
@@ -1,7 +1,9 @@
-import dataWashing
+﻿import dataWashing
 import math
 import tensorflow as tf
-
+from datetime import datetime
+import os
+import numpy as np
 
 # neural network with 5 layers
 #
@@ -21,165 +23,178 @@ import tensorflow as tf
 
 #get feature_vector as train_X, Y is corresponding label
 def load_data():
-	train_X, train_Y = dataWashing.data_to_feature()
-	return train_X, train_Y
+	# this function transfer ['类别1', '类别2', '类别1'..'类别n']共n个类别的m个训练label （train_Y） into [[0,0,0,0,1,0...共n个]...[]](共m个vecotr，vector长度为n)其中vector的第n位表示的类别n的具体是哪个类，可以由label_number_table这个字典得到。{小红：0, ..小明:n} 代表小明是vector[n]位置上的预测数字。如果这个sample是小明，在label集中这个vector[n] == 1 其他都是0。
+	
+    train_X, train_Y = dataWashing.data_to_feature()
+	label_number_table = {}
+	index = 0
+	label_num = len(set(train_Y))
+	for key in sorted(set(train_Y)):
+		label_number_table.setdefault(key, index)
+		index += 1
+	for index, y in enumerate(train_Y):
+		position = label_number_table[y]
+		train_Y[index] = [0.0 for i in range(label_num)]
+		train_Y[index][position] = 1.0
+    return train_X, train_Y, label_number_table
 
 def random_batch(train_X, train_Y, batch_size):
-	#this function create batch_size number train batch, each train time input a batch into training, instead of inputing all train data at the same time.
-	rnd_indices = np.random.randint(0,len(X_train), batch_size)
-	X_batch = train_X[rnd_indices]
-	Y_batch = train_Y[rnd_indices]
-	return X_batch, Y_batch
+    #this function create batch_size number train batch, each train time input a batch into training, instead of inputing all train data at the same time.
+    rnd_indices = np.random.randint(0,len(train_X), batch_size)
+    X_batch = train_X[rnd_indices]
+    Y_batch = train_Y[rnd_indices]
+    return X_batch, Y_batch
 
 def fc_layers(train_X, train_Y):
 
-	#feature_length = the total number of POI
-	feature_length = len(train_X[0]) 
-	label_length = len(set(train_Y))
+    #feature_length = the total number of POI
+    POI_num = int(train_X.shape[1])
+	feature_num = int(train_X.shape[2])
+    label_length = int(train_Y.shape[1])
 	
-	#five layers and their number of neurons (the last layer has label_length softmax neurons)
-	L1,L2,L3,L4,L5 = 200,100,60,30,label_length
+    #five layers and their number of neurons (the last layer has label_length softmax neurons)
+    L1,L2,L3,L4,L5 = 200,100,60,30,label_length
 
-	#weights initialized with small random values between -0.2 and 0.2
-	#When using RELUs, make sure biases are initialized with small *positive* values for example 0.1 = tf.ones([K])/10
-	W1 = tf.Variable(tf.truncated_normal([feature_length,L1],stddev = 0.1))
-	B1 = tf.Variable(tf.ones([L1])/10)
-	W2 = tf.Variable(tf.truncated_normal([L1,L2],stddev = 0.1))
-	B2 = tf.Variable(tf.ones([L2])/10)
-	W3 = tf.Variable(tf.truncated_normal([L2,L3],stddev = 0.1))
-	B3 = tf.Variable(tf.ones([L3])/10)
-	W4 = tf.Variable(tf.truncated_normal([L3,L4],stddev = 0.1))
-	B4 = tf.Variable(tf.ones([L4])/10)
-	W5 = tf.Variable(tf.truncated_normal([L4,L5],stddev = 0.1))
-	B5 = tf.Variable(tf.zeros([L5]))
+    #weights initialized with small random values between -0.2 and 0.2
+    #When using RELUs, make sure biases are initialized with small *positive* values for example 0.1 = tf.ones([K])/10
+    W1 = tf.Variable(tf.truncated_normal([feature_length,L1],stddev = 0.1))
+    B1 = tf.Variable(tf.ones([L1])/10)
+    W2 = tf.Variable(tf.truncated_normal([L1,L2],stddev = 0.1))
+    B2 = tf.Variable(tf.ones([L2])/10)
+    W3 = tf.Variable(tf.truncated_normal([L2,L3],stddev = 0.1))
+    B3 = tf.Variable(tf.ones([L3])/10)
+    W4 = tf.Variable(tf.truncated_normal([L3,L4],stddev = 0.1))
+    B4 = tf.Variable(tf.ones([L4])/10)
+    W5 = tf.Variable(tf.truncated_normal([L4,L5],stddev = 0.1))
+    B5 = tf.Variable(tf.zeros([L5]))
 
-	#the model, with dropout at each layer
-	XX = X
-	Y1 = tf.nn.relu(tf.matmul(XX,W1)+B1)
-	Y1d = tf.nn.dropout(Y1,pkeep)
+    #the model, with dropout at each layer
+    XX = tf.reshape(X,[-1, POI_num*feature_num])
+    Y1 = tf.nn.relu(tf.matmul(XX,W1)+B1)
+    Y1d = tf.nn.dropout(Y1,pkeep)
 
-	Y2 = tf.nn.relu(tf.matmul(Y1d,W2)+B2)
-	Y2d = tf.nn.dropout(Y2,pkeep)
+    Y2 = tf.nn.relu(tf.matmul(Y1d,W2)+B2)
+    Y2d = tf.nn.dropout(Y2,pkeep)
 
-	Y3 = tf.nn.relu(tf.matmul(Y2d,W3)+B3)
-	Y3d = tf.nn.dropout(Y3,pkeep)
+    Y3 = tf.nn.relu(tf.matmul(Y2d,W3)+B3)
+    Y3d = tf.nn.dropout(Y3,pkeep)
 
-	Y4 = tf.nn.relu(tf.matmul(Y3d,W4)+B4)
-	Y4d = tf.nn.dropout(Y4,pkeep)
+    Y4 = tf.nn.relu(tf.matmul(Y3d,W4)+B4)
+    Y4d = tf.nn.dropout(Y4,pkeep)
 
-	Ylogits = tf.matmul(Y4d,W5) + B5
-	Y = tf.nn.softmax(Ylogits)
-	return Ylogits
-	
-def train(data, label, learning_rate, pkeep_in, n_epochs, batch_size):
+    Ylogits = tf.matmul(Y4d,W5) + B5
+    Y = tf.nn.softmax(Ylogits)
+    return Y,Ylogits
+    
+def train(data, label, learning_rate_in, pkeep_in, n_epochs, batch_size):
+    label_length = label.shape[1]
 	test_ratio = 0.2 
-	test_size = int(len(data)* test_ratio)
-	train_X = data[:-test_size]
-	train_Y = label[:-test_size]
-	X_test = data[-test_size:]
-	Y_test = label[-test_size:]
+    test_size = int(len(data)* test_ratio)
+    train_X = data[:-test_size]
+    train_Y = label[:-test_size]
+    X_test = data[-test_size:]
+    Y_test = label[-test_size:]
 
-	n_inputs = train_X.shape[1]
-	n_ouputs = len(set(train_Y))
-	with tf.name_scope('input'):
-		X = tf.placeholder(tf.float32, shape = (None, n_inputs),name = 'x')
-		Y_label = tf.placeholder(tf.float32, shape = (None), name = 'y')
-		pkeep = tf.placeholder(tf.float32)
-		
-	Ylogits = fc_layers(X, Y_label)
-	
-	with tf.name_scope('loss'):
-		#cross-entropy loss function (= -sum(Y_i*log(Yi)) ), normalised for batches of all train X
-		#TensorFlow provides the softmax_cross_entropywith_logits function to avoid numerical stability problems with log(0)which is NaN
-		cross_entropy = tf.nn.softmax_cross_entropywith_logits(logits = Ylogits, labels = Y_label)
-		loss = tf.reduce_mean(cross_entropy, name ='loss')
-		loss_summary = tf.summary.scalar('loss', loss)
-	
-	global_step = tf.Variable(0, trainable = False)
-	with tf.name_scope('train'):
-		optimizer = tf.train.AdamOptimizer(learning_rate)
-		grads_and_vars = optimizer.compute_gradients(loss)
-		train_op = optimizer.apply_gradients(grads_and_vars, global_step = global_step)
+    n_inputs1 = len(train_X[0])
+    n_inputs2 = len(train_X[0][0])
+    with tf.name_scope('input'):
+        X = tf.placeholder(tf.float32, shape = (None, n_inputs1,n_inputs2, ),name = 'x')
+        Y_label = tf.placeholder(tf.float32, shape = (None, label_length), name = 'y')
+        pkeep = tf.placeholder(tf.float32)
+        step = tf.placeholder(tf.int32)
+    Y_pred, Ylogits = fc_layers(X, Y_label)
+    
+    with tf.name_scope('loss'):
+        #cross-entropy loss function (= -sum(Y_i*log(Yi)) ), normalised for batches of all train X
+        #TensorFlow provides the softmax_cross_entropywith_logits function to avoid numerical stability problems with log(0)which is NaN
+        cross_entropy = tf.nn.softmax_cross_entropywith_logits(logits = Ylogits, labels = Y_label)
+        loss = tf.reduce_mean(cross_entropy, name ='loss')
+        loss_summary = tf.summary.scalar('loss', loss)
+    
+    global_step = tf.Variable(0, trainable = False)
+    with tf.name_scope('train'):
+		lr = 0.0001 +  tf.train.exponential_decay(learning_rate_in, step, 2000, 1/math.e)
+		train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
 
-	with tf.name_scope('eval'):
-		predictions = tf.argmax(Ylogits,1)
-		#accuracy of the trained model, between 0(worst) and 1 (best)
-		correct = tf.nn.in_top_k(Ylogits, Y_label,1)
-		accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-		acc_summary = tf.summary.scalar('acc', accuracy)
-		
-	summary_op = tf.summary.merge([loss_summary, acc_summary])
-	
-	#write model info into checkpoint, incase it stops and then model could continue to train
-	checkpoint_path = './checkpoints/model.ckpt'
-	checkpoint_epoch_path = checkpoint_path + '.epoch'
-	final_model_path = './checkpoints/model'
+    with tf.name_scope('eval'):
+		predictions = tf.argmax(Ylogits, 1)
+        correct_prediction = tf.equal(tf.argmax(Y_label, 1), tf.argmax(Y_pred, 1))
+        #accuracy of the trained model, between 0(worst) and 1 (best)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        acc_summary = tf.summary.scalar('acc', accuracy)
+        
+    summary_op = tf.summary.merge([loss_summary, acc_summary])
+    
+    #write model info into checkpoint, incase it stops and then model could continue to train
+    checkpoint_path = './checkpoints/model.ckpt'
+    checkpoint_epoch_path = checkpoint_path + '.epoch'
+    final_model_path = './checkpoints/model'
 
-	now = datetime.utnow().strftime("%Y%m%d%H%M%S")
-	logdir = './logs' + now
-	file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
-	saver = tf.train.Saver()
-	
-	n_epochs = n_epochs
-	batch_size = batch_size
-	n_batches = int(np.ceil(len(data)/ batch_size))
+    now = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+    logdir = './logs' + now
+    file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+    saver = tf.train.Saver()
+    
+    n_epochs = n_epochs
+    batch_size = batch_size
+    n_batches = int(np.ceil(len(data)/ batch_size))
 
-	with tf.Session() as sess:
-		init = tf.global_variables_initializer()
-		
-		if os.path.isfile(checkpoint_epoch_path):
-			# if the checkpoint file exists, restore the model and load the epoch number
-			with open(checkpoint_epoch_path, 'rb') as f:
-				start_epoch = int(f.read())
-			print('Training was interuptted. Continuing at epoch', start_epoch)
-			saver.restore(sess, checkpoint_path)
-		else:
-			start_epoch = 0
-			sess.run(init)
-		
-		for epoch in range(start_epoch, n_epochs):
-		# one training time.
-			for batch_index in range(n_batches):
-			# one training time concluds n_batches times batch_size size training sample
-				X_batch, Y_batch = random_batch(train_X, train_Y, batch_size)
-				sess.run(train_op, feed_dict{X: X_batch, Y_label: Y_batch, pkeep: pkeep_in})
-			loss_val, summary_str, test_pred, test_acc = sess.run([loss, summary_op, predictions, accuracy], feed_dict{X: X_test, Y_label: Y_test, pkeep: 1.0})
-			
-			file_writer.add_summary(summary_str, epoch)
-			if epoch % 50 ==0:
-				print('Epoch:', epoch, '\tLoss:', loss_val, '\tAcc:', test_acc)
-				saver.save(sess, checkpoint_path)
-				with open(checkpoint_epoch_path, 'wb') as f:
-					f.write(b'%d' %(epoch + 1))
-		saver.save(sess, final_model_path)
-		Y_pred = predictions.eval(feed_dict = {X:X_test, y: Y_test})
-		print('precision_score', precision_score(Y_test, Y_pred))
-		print('recall_score', recall_score(Y_test, Y_pred))
-		
-		sess.close()
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        
+        if os.path.isfile(checkpoint_epoch_path):
+            # if the checkpoint file exists, restore the model and load the epoch number
+            with open(checkpoint_epoch_path, 'rb') as f:
+                start_epoch = int(f.read())
+            print('Training was interuptted. Continuing at epoch', start_epoch)
+            saver.restore(sess, checkpoint_path)
+        else:
+            start_epoch = 0
+            sess.run(init)
+        
+        for epoch in range(start_epoch, n_epochs):
+        # one training time.
+            for batch_index in range(n_batches):
+            # one training time concluds n_batches times batch_size size training sample
+                X_batch, Y_batch = random_batch(train_X, train_Y, batch_size)
+                sess.run(train_step, feed_dict = {X: X_batch, Y_label: Y_batch, pkeep: pkeep_in, step:epoch})
+            loss_val, summary_str, test_pred, test_acc = sess.run([loss, summary_op, predictions, accuracy], feed_dict = {X: X_test, Y_label: Y_test, pkeep: 1.0})
+            
+            file_writer.add_summary(summary_str, epoch)
+            if epoch % 50 ==0:
+                print('Epoch:', epoch, '\tLoss:', loss_val, '\tAcc:', test_acc)
+                saver.save(sess, checkpoint_path)
+                with open(checkpoint_epoch_path, 'wb') as f:
+                    f.write(b'%d' %(epoch + 1))
+        saver.save(sess, final_model_path)
+        Y_pred = predictions.eval(feed_dict = {X:X_test, y: Y_test})
+        print('precision_score', precision_score(Y_test, Y_pred))
+        print('recall_score', recall_score(Y_test, Y_pred))
+        
+        sess.close()
 
 
 if __name__ == '__main__':
 
-	print('TensorFlow version ' + tf.__version__)
-	tf.set_random_seed(0) # a
+    print('TensorFlow version ' + tf.__version__)
+    tf.set_random_seed(0) # a
 
-	X, Y = load_data()
-	
-	#training times
-	n_epochs = 5000
-	
-	#how many samples train at a time
-	batch_size = 64
-	
-	#probability of keeping a node during dropout, 1.0 at test time(no dropout) and 0.75 at training time
-	pkeep = 0.75
-	
-	#learning rate decay 
-	# the learning rate is : 0.0001 + 0.003 * (1/e)^(step/2000)), i.e. exponential decay from 0.003->0.0001
-	lr = 0.0001 + tf.train.exponential_decay(0.003, step, 2000, 1/math.e)
-	
-	train(X, Y, lr, pkeep, n_epochs, batch_size)
+    X, Y = load_data()
+    
+    #training times
+    n_epochs = 5000
+    
+    #how many samples train at a time
+    batch_size = 3
+    
+    #probability of keeping a node during dropout, 1.0 at test time(no dropout) and 0.75 at training time
+    pkeep = 0.75
+    
+    #learning rate decay 
+    # the learning rate is : 0.0001 + 0.003 * (1/e)^(step/2000)), i.e. exponential decay from 0.003->0.0001
+    lr = 0.003
+    
+    train(np.array(X), np.array(Y), lr, pkeep, n_epochs, batch_size)
 
 
 
--- sim_GPS_tra/code/Period2/dataWashing.py
+++ sim_GPS_tra/code/Period2/dataWashing.py
@@ -1,99 +1,99 @@
-import config
+ï»¿import config
 import numpy as np
 train_X, train_Y = [],[]
 
 def combine_fv(fv):
-	# this function is to combine a week's feature vector [{day1},{day2}...] into {day1-7}
-	res = fv[0]
-	for key in res:
-		res[key] = np.array(res[key])
-		for i in range(1,len(fv)):
-			res[key] += np.array(fv[i][key]) 
-	return res
-	
+    # this function is to combine a week's feature vector [{day1},{day2}...] into {day1-7}
+    res = fv[0]
+    for key in res:
+        res[key] = np.array(res[key])
+        for i in range(1,len(fv)):
+            res[key] += np.array(fv[i][key]) 
+    return res
+    
 def normalize(data):
-	#this function is to normalize a data(list, ultimate dimension)
-	length = len(data[0])
-	data = np.array(data,dtype = np.float32)
-	for n in range(length):
-		data[:,n][:] = data[:,n][:]/np.sum(data[:,n])
-	return data
-	
+    #this function is to normalize a data(list, ultimate dimension)
+    length = len(data[0])
+    data = np.array(data,dtype = np.float32)
+    for n in range(length):
+        data[:,n][:] = data[:,n][:]/np.sum(data[:,n])
+    return data
+    
 def judge_label(point, POI):
-	#this function is to judge which POI this point belongs to. 
-	#	1)Basic algorithm uses Python-Shapely to check if a polygon contains a point.
-	#	2)Special circumstance is that all POI don't contain this point, reason is that we ignore some area in the map when we define POI region(polygon define is not full-map-coveraged). Under this, we use min-distance to define this point's type
-	from shapely.geometry import Point
-	from shapely.geometry.polygon import Polygon
-	point_shapely = Point(point[0],point[1])
-	distance = {}# a dict to save distance between point to a polygon.
-	
-	for label in POI:
-		area = POI[label]
-		
-		if len(area) < 2:
-			print('a POI region define error, go check POI.txt\n')
-			continue
-		if len(area) == 2:# if this polygon just define by two coordinate, we judge this polygon is a rectangle defined by two coordinates
-		#First Turn Test Result£ºThis polygon contains Alg didn't work, even I judge this point is definitely in a rec box, This is because Polygon make alg is influenced by points' order. So we should adjust points' order correctly.
-			minX = min(area[0][0],area[1][0])
-			minY = min(area[0][1],area[1][1])
-			maxX = max(area[0][0],area[1][0])
-			maxY = max(area[0][1],area[1][1])
-			area = [(minX,minY),(minX,maxY),(maxX,maxY),(maxX,minY)]
+    #this function is to judge which POI this point belongs to. 
+    #	1)Basic algorithm uses Python-Shapely to check if a polygon contains a point.
+    #	2)Special circumstance is that all POI don't contain this point, reason is that we ignore some area in the map when we define POI region(polygon define is not full-map-coveraged). Under this, we use min-distance to define this point's type
+    from shapely.geometry import Point
+    from shapely.geometry.polygon import Polygon
+    point_shapely = Point(point[0],point[1])
+    distance = {}# a dict to save distance between point to a polygon.
+    
+    for label in POI:
+        area = POI[label]
+        
+        if len(area) < 2:
+            print('a POI region define error, go check POI.txt\n')
+            continue
+        if len(area) == 2:# if this polygon just define by two coordinate, we judge this polygon is a rectangle defined by two coordinates
+        #First Turn Test Resultï¿½ï¿½This polygon contains Alg didn't work, even I judge this point is definitely in a rec box, This is because Polygon make alg is influenced by points' order. So we should adjust points' order correctly.
+            minX = min(area[0][0],area[1][0])
+            minY = min(area[0][1],area[1][1])
+            maxX = max(area[0][0],area[1][0])
+            maxY = max(area[0][1],area[1][1])
+            area = [(minX,minY),(minX,maxY),(maxX,maxY),(maxX,minY)]
 
-		polygon = Polygon(area)
-		mid = np.sum(area, axis = 0)/len(area)
-		distance.setdefault(label, np.linalg.norm(mid - np.array([point[0],point[1]])))
-		if polygon.contains(point_shapely):
-			#print('region contains' + str(label)+ '\n')
-			return label
-			
-	# 2) no region contains this point.
-	#print('no region contains this point, distance calc mode.\n')
-	return min(distance, key = distance.get)
-	#attention, time to test label correctness. then think about feature vector then input it into nn.
-	
+        polygon = Polygon(area)
+        mid = np.sum(area, axis = 0)/len(area)
+        distance.setdefault(label, np.linalg.norm(mid - np.array([point[0],point[1]])))
+        if polygon.contains(point_shapely):
+            #print('region contains' + str(label)+ '\n')
+            return label
+            
+    # 2) no region contains this point.
+    #print('no region contains this point, distance calc mode.\n')
+    return min(distance, key = distance.get)
+    #attention, time to test label correctness. then think about feature vector then input it into nn.
+    
 
 def read_feature(stayPoint_path):
-	# this function open a txt file(stay point), combining with POI file, return this sp's feature vector.
-	# e.g. fv: {lib:{time: 3, minutes: 260.5}};
-	stay_points = load_stay_point(stayPoint_path)
-	POI = load_POI()
-	# now we got a stay points' list, [[lng, lat, start_time, end_time],...,[]] and a POI dict, {1:[[x1,y1],[x2,y2]]}, then we use this two structure to calc this stay point file's feature_vector
+    # this function open a txt file(stay point), combining with POI file, return this sp's feature vector.
+    # e.g. fv: {lib:{time: 3, minutes: 260.5}};
+    stay_points = load_stay_point(stayPoint_path)
+    POI = load_POI()
+    # now we got a stay points' list, [[lng, lat, start_time, end_time],...,[]] and a POI dict, {1:[[x1,y1],[x2,y2]]}, then we use this two structure to calc this stay point file's feature_vector
 
-	feature_vector = merge_sp_POI(stay_points, POI)
-	
+    feature_vector = merge_sp_POI(stay_points, POI)
+    
     return feature_vector
 
 
 def merge_sp_POI(sp, POI):
-	# this function is to merge stay_points(a list) and POI (a dict), then create a new dict that represents feature vector. 
-	# e.g. feature_vector: {1:[seconds, time]}
-	from datetime import datetime
-	# here, we use python's time lib to calc time interval. (input two time string and output a float)
-	
-	feature_vector = {}
-	for key in POI:
-		feature_vector.setdefault(key,[0.0,0])
-	
-	for point in sp:
-		# judge_label could return point label of this POI.
-		label = judge_label(point, POI)
-		time_format = '%H:%M:%S'
-		diff = datetime.strptime(point[3].split(' ')[1], time_format)  - datetime.strptime(point[2].split(' ')[1], time_format)
-		diff = diff.total_seconds()
-		feature_vector[label][0] += diff
-		feature_vector[label][1] += 1
-	return feature_vector
-	
+    # this function is to merge stay_points(a list) and POI (a dict), then create a new dict that represents feature vector. 
+    # e.g. feature_vector: {1:[seconds, time]}
+    from datetime import datetime
+    # here, we use python's time lib to calc time interval. (input two time string and output a float)
+    
+    feature_vector = {}
+    for key in POI:
+        feature_vector.setdefault(key,[0.0,0])
+    
+    for point in sp:
+        # judge_label could return point label of this POI.
+        label = judge_label(point, POI)
+        time_format = '%H:%M:%S'
+        diff = datetime.strptime(point[3].split(' ')[1], time_format)  - datetime.strptime(point[2].split(' ')[1], time_format)
+        diff = diff.total_seconds()
+        feature_vector[label][0] += diff
+        feature_vector[label][1] += 1
+    return feature_vector
+    
 def load_stay_point(filePath):
 # this function is to read a stay point file's info. input a stay_point file' path. Return sp as a list [[1,1, 9:00:00, 10:00:00 ]...[lng, lat, start, end]]
     stay_points = []
     try:
         file_obj = open(filePath)
     except Exception:
-		print('open this error' + filePath +'\n')
+        print('open this error' + filePath +'\n')
         return False
         
     for line in file_obj:
@@ -107,69 +107,69 @@ def load_stay_point(filePath):
     file_obj.close()
     
     return stay_points
-	
+    
 def load_POI(filePath = config.POI_path):
 #this function is to read POI file, input POI file, return all POI as a dict: {1:[[1,1],[2,2]]...,n:[[lng1,lat1],[lng2,lat2]]}
-	POI = {}
-	try:
-		file_obj = open(filePath)
-	except Exception:
-		print('open this error' + filePath +'\n')
-		return False
-	
-	for line in file_obj:
-		if line.split(',')[0] == 'class' or line =='\n' or line[0] == '#':
-			continue
-		label = int(line.replace('\n','').split(',')[0])
-		x1 = float(line.replace('\n','').split(',')[1])
-		y1 = float(line.replace('\n','').split(',')[2])
-		x2 = float(line.replace('\n','').split(',')[3])
-		y2 = float(line.replace('\n','').split(',')[4])
-		# improve: define POI as polygon, just modify code here. Define xn yn to save multiple coordinates, just use a for i in range(len(line.split(',')): operate_x_y)
-		POI.setdefault(label,[[x1,y1],[x2,y2]])
-	return POI
-	
-def data_to_feature(filePath = 'E:\College\´óËÄÏÂ\±ÏÉè\Êý¾Ý\Êý¾Ý·ÖÀà\Àà±ð1\\'):
-	# this function is to read a classification standard(dir-setting-based) then read all data into train_X and train_Y, 1-weeks' data as one train_X, auto-recognize .txt file name. return train_X and train_Y as lists.
-	train_X, train_Y = [],[]
-	import os
-	length_label = 0
-	filePath = config.stayPoint_path
-	label_name = os.listdir(filePath)
-	label_length = len(label_name)
-	dict_feature_with_label = {}
-	for label in sorted(label_name):
-		feature_vector = []
-		for root, dirs, files in os.walk(filePath + label):
-			if files:
-				feature_vector.append([])
-				trainData_names = sorted(list(set([file.split('gps')[0] for file in files])))
-				current_name = trainData_names[0]
-				
-				for file in sorted(files):
-					if file.split('gps')[0] != current_name:
-						trainData_names.remove(current_name)
-						current_name = file.split('gps')[0]
-						feature_vector.append([])
-						feature_vector[-1].append(read_feature(root+'\\'+file))
-						
-					else:
-						feature_vector[-1].append(read_feature(root+'\\'+file))
-		dict_feature_with_label.setdefault(label,feature_vector)	
-	
-	# here we got a feature_vector contains a week's feature day by day, now we need to combine them together to get a total-week one.
-	for label in dict_feature_with_label:
-		feature_vector = dict_feature_with_label[label]
-		for week in feature_vector:
-			res = combine_fv(week)
-			#normalize input data(train_X)
-			train_X.append(normalize(list(res.values())))
-			train_Y.append(label)
+    POI = {}
+    try:
+        file_obj = open(filePath)
+    except Exception:
+        print('open this error' + filePath +'\n')
+        return False
+    
+    for line in file_obj:
+        if line.split(',')[0] == 'class' or line =='\n' or line[0] == '#':
+            continue
+        label = int(line.replace('\n','').split(',')[0])
+        x1 = float(line.replace('\n','').split(',')[1])
+        y1 = float(line.replace('\n','').split(',')[2])
+        x2 = float(line.replace('\n','').split(',')[3])
+        y2 = float(line.replace('\n','').split(',')[4])
+        # improve: define POI as polygon, just modify code here. Define xn yn to save multiple coordinates, just use a for i in range(len(line.split(',')): operate_x_y)
+        POI.setdefault(label,[[x1,y1],[x2,y2]])
+    return POI
+    
+def data_to_feature(filePath = 'E:\College\ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½\ï¿½ï¿½ï¿½ï¿½\ï¿½ï¿½ï¿½ï¿½\ï¿½ï¿½ï¿½Ý·ï¿½ï¿½ï¿½\ï¿½ï¿½ï¿½1\\'):
+    # this function is to read a classification standard(dir-setting-based) then read all data into train_X and train_Y, 1-weeks' data as one train_X, auto-recognize .txt file name. return train_X and train_Y as lists.
+    train_X, train_Y = [],[]
+    import os
+    length_label = 0
+    filePath = config.stayPoint_path
+    label_name = os.listdir(filePath)
+    label_length = len(label_name)
+    dict_feature_with_label = {}
+    for label in sorted(label_name):
+        feature_vector = []
+        for root, dirs, files in os.walk(filePath + label):
+            if files:
+                feature_vector.append([])
+                trainData_names = sorted(list(set([file.split('gps')[0] for file in files])))
+                current_name = trainData_names[0]
+                
+                for file in sorted(files):
+                    if file.split('gps')[0] != current_name:
+                        trainData_names.remove(current_name)
+                        current_name = file.split('gps')[0]
+                        feature_vector.append([])
+                        feature_vector[-1].append(read_feature(root+'\\'+file))
+                        
+                    else:
+                        feature_vector[-1].append(read_feature(root+'\\'+file))
+        dict_feature_with_label.setdefault(label,feature_vector)	
+    
+    # here we got a feature_vector contains a week's feature day by day, now we need to combine them together to get a total-week one.
+    for label in dict_feature_with_label:
+        feature_vector = dict_feature_with_label[label]
+        for week in feature_vector:
+            res = combine_fv(week)
+            #normalize input data(train_X)
+            train_X.append(normalize(list(res.values())))
+            train_Y.append(label)
 
-					
-	return train_X,train_Y
+                    
+    return train_X,train_Y
 
-					
+                    
 #train_X: [ [feature_vector1], [fv2]... ]
 #feature_vector1: [[day1],[day2]...[day7]]
-#Ó¦¸Ã°ÑÕâ¸öfeature vectorºÏ²¢£¬ÒòÎªÃ¿ÌìÈ¥µÄµØ·½¿ÉÄÜÓÐ²îÒì¡£ÐÎ³ÉÕâ¸öÈËÕâÖÜËùÓÐÈ¥¹ýµØ·½µÄfv£¬È»ºó°ÑËùÓÐÈËºÏ²¢£¬ÐÎ³ÉËùÓÐÈËÈ¥¹ýµÄµØ·½µÄfv¡£
+#Ó¦ï¿½Ã°ï¿½ï¿½ï¿½ï¿½feature vectorï¿½Ï²ï¿½ï¿½ï¿½ï¿½ï¿½ÎªÃ¿ï¿½ï¿½È¥ï¿½ÄµØ·ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ð²ï¿½ï¿½ì¡£ï¿½Î³ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½È¥ï¿½ï¿½ï¿½Ø·ï¿½ï¿½ï¿½fvï¿½ï¿½È»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ËºÏ²ï¿½ï¿½ï¿½ï¿½Î³ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½È¥ï¿½ï¿½ï¿½ÄµØ·ï¿½ï¿½ï¿½fvï¿½ï¿½
